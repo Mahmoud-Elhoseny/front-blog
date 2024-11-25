36605cf133a3febb4d442a7ce1ec508e08a5c5b3
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2024-11-25 07:38:35 +0200

    fix:

--- src/pages/Home/AddEditTravelStory.jsx
+++ src/pages/Home/AddEditTravelStory.jsx
@@ -18,7 +18,7 @@ const AddEditTravelStory = ({
   const [image, setImage] = useState(storyInfo?.image || null);
   const [story, setStory] = useState(storyInfo?.story || '');
   const [visitedDate, setVisitedDate] = useState(
-    storyInfo?.visitedDate || null
+    storyInfo?.visitedDate.format('Do MMM YYYY') || null
   );
   const [visitedLocation, setVisitedLocation] = useState(
     storyInfo?.visitedLocation || []
